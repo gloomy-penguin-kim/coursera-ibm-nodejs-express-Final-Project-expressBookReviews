ba0b26ea79c4f7171861302c9a71af38f8a87d97
gloomy-penguin-kim <gloomy.penguin.kim@gmail.com>
2024-12-01 03:00:41 -0600

    Modified files based on project specifications

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
+const jwt = require('jsonwebtoken'); 
 const regd_users = express.Router();
 
-let users = [];
+const fs = require('fs');
+const path = require('path');
+
+const axios = require('axios') 
+
+let users = [ {username: "kim", password: "123"}];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
@@ -11,20 +15,109 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  let userswithsamename = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  return userswithsamename.length > 0; 
 }
+
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if username or password is missing
+  if (!username || !password) {
+      return res.status(404).json({ message: "Error logging in" });
+  }
+
+  // Authenticate user
+  if (authenticatedUser(username, password)) {
+      // Generate JWT access token
+      let accessToken = jwt.sign({
+          data: password
+      }, 'access', { expiresIn: 60 * 60 });
+
+      // Store access token and username in session
+      req.session.authorization = {
+          accessToken, username
+      }
+      return res.status(200).send("User successfully logged in");
+  } else {
+      return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
+
+// I had absolutely no idea how to do this with a static resource.  I thought about adding
+// mongoose and just doing it with a database or something.  This is not possible with nodemon
+// because it restarts each time the file is updated.  I am just using a json file as my 
+// database basically and the update/delete both modify that file
+
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put("/auth/review/:isbn", (req, res) => { 
+  let isbn = req.params.isbn;
+  let username = req.session.authorization.username 
+  let review = req.body.review 
+
+  axios.get('http://localhost:5000/books.json')
+    .then(response => {
+      console.log(response.data)
+      books = response.data 
+      books[isbn].reviews[username] = review 
+
+      const appDir = path.dirname(require.main.filename);
+      const filePath = path.join(appDir, 'public', 'books.json')
+
+      // this is safe for async actions (ie, if the file is being read at the "same" time)
+      fs.writeFile(filePath, JSON.stringify(books, null, 4), (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          } 
+          return res.status(200).json({message: "Review Updated for " + 
+            books[isbn].title + " by user " + username });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({message: error})
+    })
 });
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => { 
+  let isbn = req.params.isbn;
+  let username = req.session.authorization.username  
+
+  axios.get('http://localhost:5000/books.json')
+    .then(response => {
+      console.log(response.data)
+      books = response.data  
+      delete books[isbn].reviews[username]  
+
+      const appDir = path.dirname(require.main.filename);
+      const filePath = path.join(appDir, 'public', 'books.json')
+
+      // this is safe for async actions (ie, if the file is being read at the "same" time)
+      fs.writeFile(filePath, JSON.stringify(books, null, 4), (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          } 
+          return res.status(200).json({message: "Review deleted for " + 
+            books[isbn].title + " by user " + username });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({message: error})
+    })
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+
+
+ 
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,112 @@
-const express = require('express');
-let books = require("./booksdb.js");
+const express = require('express'); 
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const axios = require('axios') 
+
+const doesExist = (username) => {
+  // Filter the users array for any user with the same username
+  let userswithsamename = users.filter((user) => {
+      return user.username === username;
+  });
+  return userswithsamename.length > 0; 
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+      // Check if the user does not already exist
+      if (!doesExist(username)) {
+          // Add the new user to the users array
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: `User ${username} successfully registered. Now you can login`});
+      } else {
+          return res.status(404).json({message: `User ${username} already exists!`});
+      }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+public_users.get("/", (req,res) => {
+  axios.get('http://localhost:5000/books.json')
+    .then(response => { 
+        console.log(response.data)
+        res.json(response.data)
+    })
+    .catch(err => {
+        res.send(err)
+    }) 
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn',function (req, res) {   
+  axios.get('http://localhost:5000/books.json')
+    .then(response => {  
+      let books = response.data[req.params.isbn]
+      res.json(books)
+    })
+    .catch(err => {
+      res.send(err)
+    }) 
  });
-  
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author',function (req, res) { 
+  let author = req.params.author
+  axios.get('http://localhost:5000/books.json')
+    .then(response => {  
+      let books = response.data 
+      let byAuthor = []
+      for (isbn in books) {
+        if (books[isbn].author.toLowerCase() == author.toLowerCase()) {
+          byAuthor.push(books[isbn])
+        }
+      }
+      res.json(byAuthor)
+    })
+    .catch(err => {
+      res.send(err)
+    }) 
 });
+
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title',function (req, res) { 
+  let title = req.params.title
+  axios.get('http://localhost:5000/books.json')
+    .then(response => {  
+      let books = response.data 
+      let byTitle = []
+      for (isbn in books) {
+        if (books[isbn].title.toLowerCase() == title.toLowerCase()) {
+          byTitle.push(books[isbn])
+        }
+      }
+      res.json(byTitle)
+    })
+    .catch(err => {
+      res.send(err)
+    }) 
 });
 
+
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn',function (req, res) {   
+  axios.get('http://localhost:5000/books.json')
+    .then(response => {  
+      let bookReviews = response.data[req.params.isbn].reviews
+      res.json(bookReviews)
+    })
+    .catch(err => {
+      res.send(err)
+    }) 
 });
+
 
 module.exports.general = public_users;
